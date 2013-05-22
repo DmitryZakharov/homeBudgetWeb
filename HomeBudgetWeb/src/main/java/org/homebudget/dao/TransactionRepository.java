c1293d5f385bfb66e2314a973e1c07e02fa70009
Michael <mychajlo.wolowyk@student.hpi.uni-potsdam.de>
2013-05-22 21:28:34 +0200

    #15 Transaction repository created

--- HomeBudgetWeb/src/main/java/org/homebudget/dao/TransactionRepository.java
+++ HomeBudgetWeb/src/main/java/org/homebudget/dao/TransactionRepository.java
@@ -1,18 +1,16 @@
 package org.homebudget.dao;
 
-import java.util.List;
-import org.homebudget.model.Account;
-import org.homebudget.model.UserDetails;
+import org.homebudget.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Repository for Accounts
+ * Repository for Transactions
  * @see http://static.springsource.org/spring-data/data-jpa/docs/current/reference/htmlsingle/#jpa.query-methods
  * @see http://static.springsource.org/spring-data/commons/docs/current/reference/html/repositories.html
  */
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Long>{
-    public List<Account> findByOwner(UserDetails user);
+public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+
 
 }
